b6f07ce741f79cefde0aa2a145a2f97a2e4e8d17
imjiApril <ivoryfabric@gmail.com>
2017-11-13 16:59:43 +0900

    mon13Nov
    modal,animateProgressbar

--- fri10Nov/js/common.js
+++ fri10Nov/js/common.js
@@ -4,7 +4,28 @@ $(function(){
   //네비게이션, 컨텐츠의 사이즈 체크
   $(window).resize(function(){
     resizing();
+
+    //모달창 스크롤바
+    var height = $(window).height()-150;
+    console.log('!!!!height: ' + height);
+    $('#modal .modal-body').css({'height':height,'overflow':'auto'})
   }).resize();
+
+  //메뉴 클릭시 부드럽게 스크롤 처리
+  $('#sidenav h2 a, #sidenav .nav a, .goTop').on('click',function(event){
+
+    if(this.hash!=''){
+      event.preventDefault();
+      var hash=this.hash;
+      console.log(hash);
+      $('html, body').animate({
+        scrollTop:$(hash).offset().top
+      },1000,function(){
+        window.location.hash=hash;
+      })
+    }
+    $(this).addClass('active');
+  })
 
   function resizing(){
     var contentsH=$('#contents').height();
@@ -45,4 +66,95 @@ $(function(){
       }
     })
   })
+
+  //카테고리 필터
+  $('#works .nav a').on('click',function(event){
+    event.preventDefault();
+    //카테고리 메뉴 전체 초기화
+    $('#works .nav li').removeClass('active');
+    //선택한 메뉴 활성화
+    $(this).parent().addClass('active');
+
+var category = $(this).text();
+console.log(category);
+// var Dcategory = $('#works .item').each(function(){
+//   $(this).attr('data-category');
+// });
+// console.log(Dcategory);
+ 
+
+
+  });
+
+
+  //갤러리 팝업(모달)
+$('#works .item a').on('click',function(){
+  event.preventDefault();
+  //a태그를 기준으로 부모인 panel을 찾고 부모를 기준으로 내가 원하는 요소를 다시 찾음
+  //parent는 가장 가까운부모 s가붙으면 특정한 부모
+  var title=$(this).parents('.panel').find('.panel-title .title').text();
+  var src=$(this).attr('data-src');
+  console.log(title, src);
+  $('#modal').find('h4').text(title);
+  $('#modal').modal().find('img').attr('src',src);
+
+  //모달창 가운데 정렬/css에서 설정했음
+  // $('#modal .modal-body img').css({'margin':'auto'});
+})
+
+//팝업창 열렸을때
+  $('#modal').on('shown.bs.modal',function(){
+    $('#modal .modal-body').outerHeight($(window).innerHeight()-150);
+  })
+//팝업창 열렸을때
+$('#modal').on('hidden.bs.modal',function(){
+  $('#modal .modal-body').outerHeight('');
+})
+
+  //프로그래스바 애니메이션, 위로가기 활성화
+  $(window).scroll(function(){ //window 스크롤이 발생됬을때
+    //위로가기
+    var windowTop = $(window).scrollTop();
+    if(windowTop>100){
+      $('.goTop').addClass('on');
+    }else{
+      $('.goTop').removeClass('on');
+    }
+
+    $('#works .item').each(function(){
+      var pos = $(this).offset().top;
+      var winTop = $(window).scrollTop()+400;
+      // console.log('pos:' + pos);
+      // console.log('winTop:' + winTop);
+      if(pos<winTop){
+        //스크롤이 해당 위치와 일치하게 되면 작동
+        $(this).find('.progress-bar').each(function(){
+          //프로그래스 바의 max값을 받아오기
+          var percent = $(this).attr('aria-valuenow');
+          // console.log('percent:' + percent);
+          $(this).animate({
+            width:percent
+            },{
+              duration:1000,
+              step:function(now){
+            $(this).text(Math.floor(now)+'%');
+          },
+          //애니매이션 종료 시 줄무늬 움직이는 클라스 지우기
+          complete:function(){
+            $(this).removeClass('progress-bar-striped active');
+          }
+        })
+        })
+      }
+    })
+  }).scroll();
+/*
+  값이 소수점으로 나올때 처리방법
+  반올림 Math.round(number)
+  올림 Math.ceil(number)
+  내림 Math.floor(numcer)
+*/
+
+
+
 })
